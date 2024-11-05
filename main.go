4dbfd048277d73e3110acab90ead62f61518e85a
Pascal van Leeuwen <pascal@grove.city>
2024-11-05 19:10:47 +0000

    feat: add ability to load data from YAML

--- main.go
+++ main.go
@@ -1,21 +1,18 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"net"
 	"net/http"
 	"os"
 
 	"github.com/buildwithgrove/path/envoy/auth_server/proto"
-	"github.com/pokt-network/poktroll/pkg/polylog"
 	"github.com/pokt-network/poktroll/pkg/polylog/polyzero"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 
-	"github.com/buildwithgrove/path-auth-dataserver/postgres"
 	"github.com/buildwithgrove/path-auth-dataserver/server"
 	"github.com/buildwithgrove/path-auth-dataserver/yaml"
 
@@ -23,23 +20,24 @@ import (
 )
 
 const (
-	port                        = 50051
-	postgresConnectionStringEnv = "POSTGRES_CONNECTION_STRING"
-	yamlFilePathEnv             = "YAML_FILEPATH"
+	port            = 50051
+	yamlFilePathEnv = "YAML_FILEPATH"
 )
 
 func main() {
 	// Initialize new polylog logger
 	logger := polyzero.NewLogger()
 
-	// Load the data source from either:
-	// 1. a Postgres database
-	// 2. a YAML file
-	dataSource, cleanup, err := getDataSource(logger)
-	if err != nil {
-		panic(err)
+	yamlFilePath := os.Getenv(yamlFilePathEnv)
+	if yamlFilePath == "" {
+		panic(fmt.Errorf("YAML_FILEPATH environment variable is not set"))
 	}
-	defer cleanup()
+
+	// Load the data source from either a YAML file
+	dataSource, err := yaml.NewYAMLDataSource(yamlFilePath)
+	if err != nil {
+		panic(fmt.Errorf("failed to create YAML data source: %v", err))
+	}
 
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
@@ -80,45 +78,4 @@ func main() {
 	if err := httpServer.Serve(lis); err != nil {
 		panic(fmt.Sprintf("failed to serve: %v", err))
 	}
-}
-
-// getDataSource returns a DataSource and a cleanup function.
-// The cleanup function should be deferred to ensure resources are released.
-//
-// The specific data source loaded depends on the environment variables:
-// - POSTGRES_CONNECTION_STRING - use a Postgres database as the data source
-// - YAML_FILEPATH - use a local YAML file as the data source
-func getDataSource(logger polylog.Logger) (server.DataSource, func(), error) {
-	postgresConnectionString := os.Getenv(postgresConnectionStringEnv)
-	yamlFilePath := os.Getenv(yamlFilePathEnv)
-
-	if postgresConnectionString != "" && yamlFilePath != "" {
-		return nil, nil, fmt.Errorf("only one of POSTGRES_CONNECTION_STRING and YAML_FILEPATH can be set")
-	}
-
-	if postgresConnectionString != "" {
-		logger.Info().Str(postgresConnectionStringEnv, postgresConnectionString).Msg("Using Postgres data source")
-
-		dataSource, cleanup, err := postgres.NewPostgresDataSource(
-			context.Background(),
-			postgresConnectionString,
-			logger,
-		)
-		if err != nil {
-			return nil, nil, fmt.Errorf("failed to create Postgres data source: %v", err)
-		}
-		return dataSource, cleanup, nil
-	}
-
-	if yamlFilePath != "" {
-		logger.Info().Str(yamlFilePathEnv, yamlFilePath).Msg("Using YAML data source")
-
-		dataSource, err := yaml.NewYAMLDataSource(yamlFilePath)
-		if err != nil {
-			return nil, nil, fmt.Errorf("failed to create YAML data source: %v", err)
-		}
-		return dataSource, func() {}, nil
-	}
-
-	return nil, nil, fmt.Errorf("neither POSTGRES_CONNECTION_STRING nor YAML_FILEPATH is set")
 }
